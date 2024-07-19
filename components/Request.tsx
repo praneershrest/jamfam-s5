6286345c0417414dac49f269f879541dc9ab768a
Joshua Peng <jpa95@sfu.ca>
2024-07-18 21:32:26 -0700

    prototype request page

--- components/Request.tsx
+++ components/Request.tsx
@@ -1,13 +1,13 @@
 // components/MessageInput.tsx
 import React, { useState } from 'react'
-import { View, Dimensions, StyleSheet, Image } from 'react-native'
+import { View, Dimensions, StyleSheet, Image, Text, TouchableOpacity } from 'react-native'
 import { Video } from 'expo-av'
 import tmp from '../assets/images/josh.png'
 
 const { width, height } = Dimensions.get('window')
 
 const Request: React.FC = () => {
-  const [message, setMessage] = useState('')
+  const skills = ['JavaScript', 'React Native', 'Node.js']
 
   return (
     <View style={styles.container}>
@@ -18,8 +18,38 @@ const Request: React.FC = () => {
         shouldPlay
         isLooping
       />
-      <View style={styles.profileContainer}>
-        <Image source={tmp} style={styles.profilePicture} />
+
+      <View style={styles.profileSection}>
+        <View style={styles.profileContainer}>
+          <Image source={tmp} style={styles.profilePicture} />
+        </View>
+        <View style={styles.descriptionContainer}>
+          <Text style={styles.profileName}>PengusJams</Text>
+          <Text style={styles.profileDescription}>Singer songwriter </Text>
+          <View style={styles.skillsContainer}>
+            {skills.map((skill, index) => (
+              <View key={index} style={styles.skillPill}>
+                <Text style={styles.skillText}>{skill}</Text>
+              </View>
+            ))}
+          </View>
+          <View style={styles.descriptionBox}>
+            <Text style={styles.descriptionText}>
+              {'\u2022'} Experienced using DAWs like FL Studio, Ableton Live, and Logic Pro X{'\n'}
+              {'\u2022'} I like the post mixing/mastering process{'\n'}
+              {'\u2022'} Can sing too
+            </Text>
+          </View>
+          <Text style={styles.headerText}>Let's Jam, Fam?</Text>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Yes 🤝</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>No 🗑️</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     </View>
   )
@@ -27,7 +57,6 @@ const Request: React.FC = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     flexDirection: 'column',
   },
   video: {
@@ -35,20 +64,86 @@ const styles = StyleSheet.create({
     height: height / 2.5,
   },
   profileContainer: {
-    position: 'absolute',
-    top: height / 2 - 100, // Adjust this value to center the profiles
-    left: 0,
-    right: 0,
-    height: 100,
     flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: -40,
+  },
+  descriptionContainer: {
+    padding: 20,
     alignItems: 'center',
   },
   profilePicture: {
     width: 100,
     height: 100,
     borderRadius: 25,
-    margin: 10,
+  },
+  profileSection: {
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+  },
+  profileName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  profileDescription: {
+    fontSize: 16,
+    color: '#666',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  skillsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  skillPill: {
+    backgroundColor: '#3498db',
+    borderRadius: 20,
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+    margin: 5,
+  },
+  skillText: {
+    color: 'white',
+    fontSize: 14,
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    marginVertical: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 5,
+  },
+  button: {
+    backgroundColor: '#3498db',
+    borderRadius: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginHorizontal: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  descriptionBox: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 15,
+    marginTop: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    width: '90%',
+  },
+  descriptionText: {
+    fontSize: 16,
+    color: '#666',
   },
 })
 
