fa9ff89e9b0d380152d66e6a336c0240e2425457
joshpeng1999 <jpa95@sfu.ca>
2024-07-25 23:59:04 -0700

    update profile page

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,39 +1,77 @@
 // components/Profile.tsx
-import React, { useEffect, useState, useRef } from 'react';
-import { View, Dimensions, StyleSheet, Image, Text, TouchableOpacity, ScrollView, FlatList, Modal } from 'react-native';
-import { Video, ResizeMode } from 'expo-av';
-import josh from '../assets/images/josh.png';
-import joyang from '../assets/images/joyang.png';
-import praneer from '../assets/images/praneer.png';
-import joyangVideo from '../assets/videos/joyang.mp4';
-import idol from '../assets/videos/idol.mp4';
-import rapbeat from '../assets/videos/rapbeat.mp4';
+import React, { useEffect, useState, useRef } from 'react'
+import {
+  View,
+  Dimensions,
+  StyleSheet,
+  Image,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  FlatList,
+  Modal,
+  Linking,
+} from 'react-native'
+import { Video, ResizeMode } from 'expo-av'
+import josh from '../assets/images/josh.png'
+import joyang from '../assets/images/joyang.png'
+import praneer from '../assets/images/praneer.png'
+import joyangVideo from '../assets/videos/joyang.mp4'
+import idol from '../assets/videos/idol.mp4'
+import rapbeat from '../assets/videos/rapbeat.mp4'
+import vinly from '../assets/images/vinyl.png'
+import vinly2 from '../assets/images/vinly2.png'
+import vinly3 from '../assets/images/vinly3.png'
+import wolf from '../assets/images/wolf.png'
+import me_img from '../assets/images/me_img.png'
+import eleven_cover from '../assets/images/eleven_cover.png'
 
-const { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get('window')
 
 const profileInfo = {
   '1': {
     name: 'PengusJams',
     profilePic: josh,
     title: 'Producer SongWriter',
-    profileVid: idol,
-    skills: ['Guitar', 'Fl studio', 'Mixing'],
+    profileVid: rapbeat,
+    skills: ['Guitar', 'Fl studio', 'Mixing', 'Choreographing', 'Video Editing', 'Songwriting'],
     bio: "Hello 👋 I'm PengusJams, an emerging artist from Vancouver, Canada. I'm currently learning Logic Pro X for music production and enjoy creating R&B tracks. I'm always eager to learn new techniques and explore various genres. If you have any R&B projects, I'd love to assist with production or songwriting. Let's connect!",
     projects: [
-        { id: '1', title: 'Midsummer madness', snippet: idol },
-        { id: '2', title: 'Song #2', snippet: joyangVideo },
+      {
+        id: '1',
+        title: 'Snippet #1',
+        snippet: idol,
+        projectImg: vinly,
+        projectImgBig: eleven_cover,
+      },
+      {
+        id: '2',
+        title: 'Snippet #2',
+        snippet: joyangVideo,
+        projectImg: vinly2,
+        projectImgBig: wolf,
+      },
+      {
+        id: '3',
+        title: 'Snippet #3',
+        snippet: joyangVideo,
+        projectImg: vinly3,
+        projectImgBig: me_img,
+      },
     ],
     favoriteGenres: ['Pop', 'Soul', 'Classical'],
+    personality: ['Likes to be at home', 'Introverted', 'Gaming', 'Watching anime', 'Cooking'],
     socials: {
-        instagram: 'https://www.instagram.com/pengusjams/',
-        spotify: 'https://open.spotify.com/artist/pengusjams'
-    }
+      instagram: 'https://www.instagram.com/pengusjams/',
+      spotify: 'https://open.spotify.com/artist/pengusjams',
+    },
   },
   '2': {
     name: 'Joyang',
     profilePic: joyang,
     title: 'Singer Producer',
     profileVid: joyangVideo,
+    profileImg: vinly2,
     skills: ['Piano', 'Vocals', 'Logic Pro'],
     favoriteGenres: ['Pop', 'Soul', 'Classical'],
     bio: 'Joyang is a talented singer and producer who blends soulful melodies with innovative production techniques.',
@@ -43,51 +81,56 @@ const profileInfo = {
     profilePic: praneer,
     title: 'Rapper',
     profileVid: rapbeat,
+    profileImg: vinly3,
     skills: ['Rapping', 'Songwriting', 'Performance'],
     favoriteGenres: ['Pop', 'Soul', 'Classical'],
     bio: 'Lil Boat is a dynamic rapper known for his energetic performances and thought-provoking lyrics.',
   },
-};
+}
 
 interface ProfileProps {
-  id: string;
+  id: string
 }
 
 const Profile: React.FC<ProfileProps> = ({ id }) => {
-    const [selectedProfile, setSelectedProfile] = useState<any>(null);
-    const [isModalVisible, setModalVisible] = useState(false);
-    const [selectedVideo, setSelectedVideo] = useState(null);
-    const videoRef = useRef<Video>(null);
-    const feedVideoRef = useRef<Video>(null);
+  const [selectedProfile, setSelectedProfile] = useState<any>(null)
+  const [isModalVisible, setModalVisible] = useState(false)
+  const [selectedVideo, setSelectedVideo] = useState(null)
+  const videoRef = useRef<Video>(null)
+  const feedVideoRef = useRef<Video>(null)
 
   useEffect(() => {
     if (id in profileInfo) {
-      setSelectedProfile(profileInfo[id]);
+      setSelectedProfile(profileInfo[id])
     }
     return () => {
       if (videoRef.current) {
-        videoRef.current.pauseAsync();
-        feedVideoRef.current.pauseAsync();
+        videoRef.current.pauseAsync()
+        feedVideoRef.current.pauseAsync()
       }
-    };
-  }, [id]);
+    }
+  }, [id])
 
   const openModal = (video) => {
-    setSelectedVideo(video);
-    setModalVisible(true);
-  };
+    setSelectedVideo(video)
+    setModalVisible(true)
+  }
 
   const closeModal = () => {
-    setModalVisible(false);
-    setSelectedVideo(null);
-  };
+    setModalVisible(false)
+    setSelectedVideo(null)
+  }
 
   return (
     <ScrollView style={styles.container}>
       {selectedProfile && (
         <>
           <Video
-            source={selectedProfile.profileVid ? selectedProfile.profileVid : { uri: 'https://www.w3schools.com/html/mov_bbb.mp4' }}
+            source={
+              selectedProfile.profileVid
+                ? selectedProfile.profileVid
+                : { uri: 'https://www.w3schools.com/html/mov_bbb.mp4' }
+            }
             style={styles.video}
             resizeMode={ResizeMode.COVER}
             shouldPlay
@@ -105,6 +148,7 @@ const Profile: React.FC<ProfileProps> = ({ id }) => {
             <Text style={styles.bioTitle}>About Me:</Text>
             <Text style={styles.bioText}>{selectedProfile.bio}</Text>
           </View>
+          <Text style={styles.headerText}>Skills</Text>
           <View style={styles.skillsContainer}>
             {selectedProfile.skills.map((skill: string, index: number) => (
               <View key={index} style={styles.skillPill}>
@@ -112,78 +156,75 @@ const Profile: React.FC<ProfileProps> = ({ id }) => {
               </View>
             ))}
           </View>
-            <Text style={styles.headerText}>Project Snippets</Text>
+          <Text style={styles.headerText}>Project Snippets</Text>
 
-            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-              {selectedProfile.projects.map((project: any) => (
-                <TouchableOpacity onPress={() => openModal(project.snippet)}>
-                  <Video
-                    source={project.snippet}
-                    style={styles.projectVideo}
-                    resizeMode={ResizeMode.COVER}
-                    shouldPlay
-                    isLooping
-                  />
-                  <Text style={styles.projectTitle}>{project.title}</Text>
-                </TouchableOpacity>
-              ))}
-            </ScrollView>
-            <Text style={styles.headerText}>Completed Projects</Text>
-            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-              {selectedProfile.projects.map((project: any) => (
-                <TouchableOpacity onPress={() => openModal(project.snippet)}>
-                  <Video
-                    source={project.snippet}
-                    style={styles.projectSnippetVideo}
-                    resizeMode={ResizeMode.COVER}
-                    shouldPlay
-                    isLooping
-                  />
-                  <Text style={styles.projectTitle}>{project.title}</Text>
-                </TouchableOpacity>
-              ))}
-            </ScrollView>
-            
-            <Text style={styles.headerText}>Favorite Music Genres</Text>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {selectedProfile.projects.map((project: any) => (
+              <TouchableOpacity onPress={() => openModal(project.snippet)}>
+                <Image source={project.projectImg} style={styles.projectVideo} resizeMode="cover" />
+                <Text style={styles.projectTitle}>{project.title}</Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+          <Text style={styles.headerText}>Completed Projects</Text>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {selectedProfile.projects.map((project: any) => (
+              <TouchableOpacity onPress={() => openModal(project.snippet)}>
+                <Image
+                  source={project.projectImgBig}
+                  style={styles.projectSnippetVideo}
+                  resizeMode="cover"
+                />
+                <Text style={styles.projectTitle}>{project.title}</Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
 
-            <View style={styles.skillsContainer}>
-                {selectedProfile.favoriteGenres.map((genre: string, index: number) => (
-                <View key={index} style={styles.skillPill}>
-                    <Text style={styles.genreText}>{genre}</Text>
-                </View>
-                ))}
+          <Text style={styles.headerText}>Favorite Music Genres</Text>
+          <View style={styles.skillsContainer}>
+            {selectedProfile.favoriteGenres.map((genre: string, index: number) => (
+              <View key={index} style={styles.skillPill}>
+                <Text style={styles.genreText}>{genre}</Text>
+              </View>
+            ))}
+          </View>
+          <Text style={styles.headerText}>Personality/Hobbies</Text>
+          <View style={styles.skillsContainer}>
+            {selectedProfile.personality.map((skill: string, index: number) => (
+              <View key={index} style={styles.skillPill}>
+                <Text style={styles.skillText}>{skill}</Text>
+              </View>
+            ))}
+          </View>
+          <Text style={styles.headerText}>Socials</Text>
+          <View style={styles.socialsContainer}>
+            <TouchableOpacity onPress={() => Linking.openURL(selectedProfile.socials.instagram)}>
+              <Text style={styles.socialLink}>Instagram</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => Linking.openURL(selectedProfile.socials.spotify)}>
+              <Text style={styles.socialLink}>Spotify</Text>
+            </TouchableOpacity>
+          </View>
+
+          <Modal visible={isModalVisible} transparent={true} onRequestClose={closeModal}>
+            <View style={styles.modalContainer}>
+              <Video
+                source={selectedVideo}
+                style={styles.modalVideo}
+                resizeMode={ResizeMode.CONTAIN}
+                shouldPlay
+                useNativeControls
+              />
+              <TouchableOpacity style={styles.closeButton} onPress={closeModal}>
+                <Text style={styles.closeButtonText}>Close</Text>
+              </TouchableOpacity>
             </View>
-            {/* Socials Section */}
-            <Text style={styles.headerText}>Socials</Text>
-            <View style={styles.socialsContainer}>
-                <TouchableOpacity onPress={() => Linking.openURL(selectedProfile.socials.instagram)}>
-                    <Text style={styles.socialLink}>Instagram</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => Linking.openURL(selectedProfile.socials.spotify)}>
-                    <Text style={styles.socialLink}>Spotify</Text>
-                </TouchableOpacity>
-            </View>
-
-            <Modal visible={isModalVisible} transparent={true} onRequestClose={closeModal}>
-                <View style={styles.modalContainer}>
-                    <Video
-                        source={selectedVideo}
-                        style={styles.modalVideo}
-                        resizeMode={ResizeMode.CONTAIN}
-                        shouldPlay
-                        useNativeControls
-                    />
-                    <TouchableOpacity style={styles.closeButton} onPress={closeModal}>
-                        <Text style={styles.closeButtonText}>Close</Text>
-                    </TouchableOpacity>
-                </View>
-            </Modal>
-
+          </Modal>
         </>
       )}
     </ScrollView>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -272,6 +313,7 @@ const styles = StyleSheet.create({
     marginTop: 5,
     fontSize: 14,
     color: '#333',
+    textAlign: 'center',
   },
   favoriteGenresContainer: {
     padding: 20,
@@ -342,6 +384,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginHorizontal: 10,
   },
-});
+})
 
-export default Profile;
+export default Profile
